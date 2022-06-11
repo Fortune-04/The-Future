892528f91b369cf4ed5bf7c785ca467d04d29d57
Fortune-04 <spiritlance48@gmail.com>
2022-06-11 18:15:03 +0800

    Create Dashboard UI

--- TheFuture/MainWindow.xaml.cs
+++ TheFuture/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace TheFuture
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
     }
 }
